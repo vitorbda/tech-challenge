df00f6ac8f7e0faab17d4f671638a213ccefe595
vitorbda <joaovitorveloz@gmail.com>
2026-08-30 22:36:27 -0300

    [3-4] Inexistência do índice único de CPF
    Aplicação do Migrations após alterações de índice único por CPF

---
+++ base/backend-dotnet/src/Desafio.Api/Infraestrutura/Migrations/20260831011426_AdicionaIndiceUnicoCpfBeneficiarios.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Desafio.Api.Infraestrutura.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionaIndiceUnicoCpfBeneficiarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Beneficiarios_Cpf",
+                table: "Beneficiarios",
+                column: "Cpf",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Beneficiarios_Cpf",
+                table: "Beneficiarios");
+        }
+    }
+}
