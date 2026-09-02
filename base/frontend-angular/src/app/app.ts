ef058bdc0c79fb807d21c8429e456a931e9b0d45
vitorbda <joaovitorveloz@gmail.com>
2026-09-02 08:27:50 -0300

    [6-4] Listagem de beneficiários com filtros combináveis e paginação
    Realiza importação de BeneficiariosLista no app.ts

--- base/frontend-angular/src/app/app.ts
+++ base/frontend-angular/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+import { BeneficiariosLista } from './beneficiarios/beneficiarios-lista';
 import { PlanosLista } from './planos/planos-lista';
 
 @Component({
   selector: 'app-root',
-  imports: [PlanosLista],
+  imports: [PlanosLista, BeneficiariosLista],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
